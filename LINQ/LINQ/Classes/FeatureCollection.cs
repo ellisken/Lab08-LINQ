590fa557e326a1f063a9596cb5a9f8b970a02da1
ellisken <kse104@gmail.com>
2018-10-19 09:26:08 -0700

    define classes

---
+++ LINQ/LINQ/Classes/FeatureCollection.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LINQ.Classes
+{
+    public class RootObject
+    {
+        public List<Feature> features { get; set; }
+    }
+}
